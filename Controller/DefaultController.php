4bf55f71f28cb0a5d37a77c8309bd9d9cc2c082a
Eric Angenault <eat@vaudoise.ch>
2016-08-05 16:56:30 +0200

    Creation du job, du script et deploiement

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,11 +10,53 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AriiBlocklyBundle:Default:index.html.twig' );
+        $level = 1;
+        return $this->render('AriiBlocklyBundle:Default:index.html.twig', array('level' => $level, 'scripts'=> array()) );
+    }
+
+    public function editAction($level=0)
+    {
+        $request = Request::createFromGlobals();
+        if ($request->get('level')>0)
+            $level = $request->get('level');
+        
+        // recuperation des scripts
+        $folder = $this->container->get('arii_core.folder');
+        $basedir = $this->container->getParameter('workspace').'/Blockly/JobScheduler';
+        $Files = $folder->FilesList($basedir ,'',array('*','!code','!xml'));
+        $Scripts = array();
+        foreach ($Files as $file) {
+            $Scripts[$file]['file'] = $file;
+            $Scripts[$file]['label'] = str_replace(array('/'),array('_'),$file);
+            $Scripts[$file]['code'] = base64_encode(str_replace("\r\n","\n",file_get_contents("$basedir/$file")));
+            $p = strrpos($file,'.');
+            $ext = substr($file,$p+1);
+            switch($ext) {
+                case 'pl':
+                    $Scripts[$file]['lang'] = 'perlscript';
+                    break;
+                case 'js':
+                    $Scripts[$file]['lang'] = 'javascript';
+                    break;
+                case 'jar':
+                    $Scripts[$file]['lang'] = 'java';
+                    break;
+                case 'cmd':
+                case 'bat':
+                    $Scripts[$file]['lang'] = 'bat';
+                    break;
+                default:
+                    $Scripts[$file]['lang'] = 'shell';
+            }            
+        }
+        return $this->render('AriiBlocklyBundle:Default:index.html.twig', array('level' => $level,'scripts'=>$Scripts) );
     }
     
     public function ribbonAction()
     {
+        $request = Request::createFromGlobals();
+        $level = $request->get('level');
+        
         $folder = $this->container->get('arii_core.folder');
         $session = $this->container->get('arii_core.session');
         $engine = $session->getSpoolerByName('arii');
@@ -28,7 +70,7 @@ class DefaultController extends Controller
         
         $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
-        return $this->render('AriiBlocklyBundle:Default:ribbon.json.twig',array('Schedulers' => $Dir), $response);
+        return $this->render('AriiBlocklyBundle:Default:ribbon.json.twig',array('level' => $level, 'Schedulers' => $Dir), $response);
     }
 
     public function readmeAction()
@@ -56,14 +98,20 @@ class DefaultController extends Controller
         $name = $request->get('name');
         if ($name=='')
             $name = 'saved';
-        $xml = $request->get('xml');        
-        $code = $request->get('code');    
+        $xml = $request->get('xml');
+        $code = $request->get('code');
+
+        $root = $this->container->getParameter('workspace').'/Blockly/JobScheduler';
         
-        $file = $this->container->getParameter('workspace').'/Blockly/'.$name.".xml";
-        if (file_put_contents($file,$xml)) {
-            print $name;
-            $file = $this->container->getParameter('workspace').'/Blockly/'.$name.".code";
-            file_put_contents($file,$code);            
+        // Creation de l'arborescence
+        $Paths = explode('/',$name);
+        $filename = array_pop($Paths);
+        $dir = $this->CreatePath($root,$Paths);
+        
+        print "$filename -> $dir</br>";
+        
+        if (file_put_contents("$dir/$filename.xml",$xml)) {
+            file_put_contents("$dir/$filename.code",$code);            
         }
         else {
             print "ERROR!";
@@ -71,6 +119,15 @@ class DefaultController extends Controller
         exit();
     }
 
+    private function CreatePath($root,$Paths) {        
+        $path = $root;
+        foreach ($Paths as $p) {
+            $path .= "/$p";
+            @mkdir($path);
+        }
+        return $path;
+    }
+    
     public function checkAction()
     {
         $request = Request::createFromGlobals();
@@ -82,11 +139,17 @@ class DefaultController extends Controller
     public function deployAction()
     {        
         $request = Request::createFromGlobals();
+        $name = $request->get('name');
         $code = $request->get('code');
         
+        print "<b>$name</b><br/>\n";
         $ojs = $this->container->get('arii_blockly.jobscheduler');
-        $live = $ojs->getLive();
-        print $this->split($code,$live);        
+        $live = $ojs->getLive();   
+        $Paths = explode('/',$name);
+        $version = array_pop($Paths);
+        $dir = $this->CreatePath($live,$Paths);
+
+        print $this->split($code,$dir);        
         exit();
     }
     
@@ -118,7 +181,8 @@ class DefaultController extends Controller
         $request = Request::createFromGlobals();
         $name = $request->get('name');
         
-        $file = $this->container->getParameter('workspace').'/Blockly/'.$name.".xml";
+        $file = $this->container->getParameter('workspace').'/Blockly/JobScheduler/'.$name.'.xml';
+
         $response = new Response();
         $response->headers->set('Content-Type', 'text/xml');
         $response->setContent( file_get_contents($file) );
@@ -131,14 +195,14 @@ class DefaultController extends Controller
         $response->headers->set('Content-Type', 'text/xml');
         $list = '<?xml version="1.0" encoding="UTF-8"?>';
         $list .= '<rows>';        
-        $basedir = $this->container->getParameter('workspace').'/Blockly';
-        if ($dh = @opendir($basedir)) {
-            while (($file = readdir($dh)) !== false) {    
-                if (substr($file,-4) == '.xml') {
-                    $xml = substr($file,0,strlen($file)-4);
-                    $list .= '<row id="'.$xml.'"><cell>'.$xml.'</cell></row>';
-                }
-            }
+       
+        $folder = $this->container->get('arii_core.folder');
+        $basedir = $this->container->getParameter('workspace').'/Blockly/JobScheduler';
+        $Files = $folder->FilesList($basedir ,'',array('xml'));
+
+        foreach ($Files as $file) {
+            $f = substr($file,0,strlen($file)-4);
+            $list .= '<row id="'.$f.'"><cell>'.$f.'</cell></row>';
         }
         $list .= '</rows>';
 
---
+++ Controller/ScriptsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Arii\BlocklyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class ScriptsController extends Controller
+{
+    
+    public function indexAction()
+    {
+        return $this->render('AriiBlocklyBundle:Scripts:index.html.twig' );
+    }
+
+    public function listAction()
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml');
+        $list = '<?xml version="1.0" encoding="UTF-8"?>';
+        $list .= '<rows>';        
+       
+        $folder = $this->container->get('arii_core.folder');
+        $basedir = $this->container->getParameter('workspace').'/Blockly/JobScheduler';
+        $Files = $folder->FilesList($basedir ,'',array('*','!code','!xml'));
+
+        foreach ($Files as $file) {
+            $list .= '<row id="'.$file.'"><cell>'.$file.'</cell></row>';
+        }
+        $list .= '</rows>';
+
+        $response->setContent( $list );
+        return $response;        
+    }
+    
+    
+}
